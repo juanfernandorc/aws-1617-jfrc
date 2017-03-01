06b0adc54b2274b14b77f185d2cda3760b6089be
Juan Fernando Rivas Castillo <juanfernandorc@gmail.com>
2017-03-01 18:16:47 +0000

    Initial version of server

---
+++ index.js
@@ -0,0 +1,27 @@
+var cool = require("cool-ascii-faces");
+var express = require("express");
+var port = ( process.env.PORT || 3000);
+var app = express();
+var baseAPI = "/api/v1";
+
+var contacts = [
+    {name: "pepe", phone:"12345", email:"pepe@pepe.com"},
+    {name: "luis", phone:"6789", email:"luis@luis.com"}
+    ];
+//console.log(cool());
+
+app.get(baseAPI+"/contacts", (request,response) => {
+    response.send(contacts);
+   console.log("GET /contacts");
+});
+
+app.post(baseAPI+"/contacts", (request,response) => {
+    var contact = request.body;
+    contacts.push(contact);
+    response.sendStatus(201);
+   console.log("POST /contacts");
+})
+
+app.listen(port, () => {
+    console.log("Server up and running!");
+});
