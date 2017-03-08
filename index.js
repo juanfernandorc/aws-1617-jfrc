da4c99878a2995b26f8240dc5e5d0984d082c240
Juan Fernando Rivas Castillo <juanfernandorc@gmail.com>
2017-03-08 16:18:00 +0000

    08/03

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require('path');
+var dataStore = require('nedb');
+var dbFileName = path.join(__dirname, 'contacts.json');
+
+var db = new dataStore({
+    filename : dbFileName,
+    autoload : true
+});
 
 var port = (process.env.PORT || 10000);
-var baseApi = "/api/v1";
-var dbFileName = path.join(__dirname, 'contacts.json');
 
 var app = express();
 app.use(bodyParser.json());
 
-var port = (process.env.PORT || 3000);
+var port = (process.env.PORT || 3000);  
 
 var baseAPI = "/api/v1";
 
-var contacts = [];
+//var contacts = [];
 
 app.get(baseAPI + "/contacts", (request, response) => {
-    response.send(contacts);
+    // var contacts;
+    
+    db.find({}, (err, contacts)=> {
+        response.send(contacts);
+    });
+    
     console.log("GET /contacts");
 });
 
 app.post(baseAPI + "/contacts", (request, response) => {
 
     var contact = request.body;
-    contacts.push(contact);
+    db.insert(contact);
 
     response.sendStatus(201);
 
